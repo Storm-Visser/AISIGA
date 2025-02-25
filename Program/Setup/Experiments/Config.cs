9107bbf5539118a506cc6cdcd61fc21fbaf1db13
Storm Visser <storm.visser@student.nhlstenden.com>
2025-02-25 16:38:40 +0100

    Setup stuff

---
+++ Program/Setup/Experiments/Config.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISIGA.Program.Setup.Experiments
+{
+    class Config
+    {
+    }
+}
--- Program/Setup/Setup.cs
+++ Program/Setup/Setup.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AISIGA.Program.Setup.Experiments;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,14 @@ using System.Threading.Tasks;
 
 namespace AISIGA.Program.Setup
 {
+
     class Setup
     {
+        private readonly Config ExperimentConfig;
+
+        public Setup(string ExperimentNumber)
+        {
+
+        }
     }
 }
