f3d8900d1d4f80f0146825f97daa4bc96b59ec74
Storm Visser <storm.visser@student.nhlstenden.com>
2025-02-25 16:24:36 +0100

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace AISIGA;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    //Start point of the code
+}
+
