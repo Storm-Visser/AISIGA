29dcbde024dbafff5c122fe075e4ce1851b8c251
Storm Visser <storm.visser@student.nhlstenden.com>
2025-04-16 15:51:55 +0200

    test class added so i dont forget

---
+++ Program/Tests/MethodTests/DetectionOfAGByABTest.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AISIGA.Program.Tests.MethodTests
+{
+    class DetectionOfAGByABTest
+    {
+        //Todo: Implement the test for the Detection of an AG by an AB in FitnessFunctions.IsAntigenMatched().
+    }
+}
