1ca972e4bb91b6ac7a2d8e3ca17218cd67ed3c88
Storm Visser <storm.visser@student.nhlstenden.com>
2026-04-13 15:31:26 +0200

    TTest results

--- Results/T-Testing/TTest.py
+++ Results/T-Testing/TTest.py
@@ -1,11 +1,11 @@
 ﻿from scipy.stats import ttest_rel
 
-TestAcc = [46.67, 49.89, 51.63, 51.67, 50.62]
-Time = [4.56, 4.58, 4.64, 4.65, 4.79]
+TestAcc = [64.94, 64.94, 64.94, 65.36, 65.36]
+Time = [16.35, 16.37, 16.25, 16.26, 16.23]
 TestAcc1 = TestAcc
 Time1 = Time
-TestAcc = [48.89, 51.93, 50.81, 50.48, 54.0]
-Time = [0.43, 0.4, 0.41, 0.4, 0.41]
+TestAcc = [64.94, 64.94, 64.94, 65.36, 65.36]
+Time = [607.89, 601.26, 607.45, 602.53, 600.29]
 
 t_stat1, p_value1 = ttest_rel(TestAcc, TestAcc1)
 t_stat2, p_value2 = ttest_rel(Time, Time1)
